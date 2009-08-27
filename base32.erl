29e7ae5c8345cef948b164b39bf2da15514ddd07
John Kemp <john@jkemp.net>
2009-08-26 22:33:18 -0400

    added license header

--- base32.erl
+++ base32.erl
@@ -1,3 +1,41 @@
+%%% @author  John Kemp <john@jkemp.net> [http://jkemp.net/]
+%%% @doc Implementation of z-base-32 in Erlang.
+%%% @reference See <a href="http://zooko.com/repos/z-base-32/base32/DESIGN">Z-Base-32</a>. Find the code <a href="http://github.com/frumioj/erl-base">here</a>.
+%%% @since 26 August 2009
+%%%
+%%% @copyright 2009 John Kemp, All rights reserved. Open source, BSD License
+%%% @version 1.1
+%%%
+
+%%%
+%%% Copyright (c) 2009 John Kemp
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without
+%%% modification, are permitted provided that the following conditions
+%%% are met:
+%%% 1. Redistributions of source code must retain the above copyright
+%%%    notice, this list of conditions and the following disclaimer.
+%%% 2. Redistributions in binary form must reproduce the above copyright
+%%%    notice, this list of conditions and the following disclaimer in the
+%%%    documentation and/or other materials provided with the distribution.
+%%% 3. Neither the name of the copyright holder nor the names of contributors
+%%%    may be used to endorse or promote products derived from this software
+%%%    without specific prior written permission.
+%%% 
+%%% THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTOR(S) ``AS IS'' AND
+%%% ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%%% ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTOR(S) BE LIABLE
+%%% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+%%% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+%%% OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+%%% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+%%% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+%%% OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+%%% SUCH DAMAGE.
+%%%
+
 -module(base32).
 
 -export([encode/1, decode/1,
